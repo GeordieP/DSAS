78d74232d990842c48756faeec1d91d01c401fa6
GeordieP <geordie.powers@gmail.com>
2016-05-01 00:43:55 -0400

    Player moves based on touch input. Holding two fingers for one second will call player's bomb ability method (not yet implemented)

---
+++ Assets/Scripts/Gameplay/PlayerInput.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+	// Movement directions 
+	private const System.SByte NONE = 0;
+	private const System.SByte RIGHT = 1;
+	private const System.SByte LEFT = -1;
+	// Movement direction - basically just simplifies setting the sign of movement velocity (negative = left)
+	private System.SByte MOVE_DIR = NONE;
+	private const float moveSpeed = 10;
+	private float halfScreenWidth;
+
+	private const float SMOOTHING_TIME_ACCELERATING = 0.04f;
+	private const float SMOOTHING_TIME_DECELERATING = 0.06f;
+	private float SMOOTHING_TIME = SMOOTHING_TIME_ACCELERATING;
+
+	private float velocity, targetVelocity;
+	private float velocitySmoothing;
+
+	Timer bombTriggerDelayTimer;
+	float bombTriggerDelayDuration = 1f;
+
+	// Use this for initialization
+	void Start() {
+		halfScreenWidth = Screen.width / 2;
+		bombTriggerDelayTimer = TimerManager.Instance.CreateTimerOneshot(bombTriggerDelayDuration);
+		bombTriggerDelayTimer.onFinish += bombTriggerDelayTimer_onFinish;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		// user is touching screen
+		if (Input.touches.Length > 0) {
+			// more than one finger
+			if (Input.touches.Length > 1) {
+				// stop movement, start bomb timer
+				MOVE_DIR = NONE;
+				SMOOTHING_TIME = SMOOTHING_TIME_DECELERATING;
+				if (!bombTriggerDelayTimer.running)
+					bombTriggerDelayTimer.Start();
+			} else {
+				// only one finger
+				// move appropriate direction and stop bomb timer if it's running
+				MOVE_DIR = (Input.GetTouch(Input.touches.Length - 1).position.x < halfScreenWidth) ? LEFT : RIGHT;
+				SMOOTHING_TIME = SMOOTHING_TIME_ACCELERATING;
+
+				if (bombTriggerDelayTimer.running)
+					bombTriggerDelayTimer.Stop();
+			}
+		} else {
+			// no fingers on screen
+			MOVE_DIR = NONE;
+			SMOOTHING_TIME = SMOOTHING_TIME_DECELERATING;
+
+			if (bombTriggerDelayTimer.running)
+				bombTriggerDelayTimer.Stop();
+		}
+
+		// Move character based on input. Smooth out acceleration using SmoothDamp
+		targetVelocity = MOVE_DIR * moveSpeed;
+		velocity = Mathf.SmoothDamp(velocity, targetVelocity, ref velocitySmoothing, SMOOTHING_TIME);
+		transform.Translate(Vector3.right * velocity * Time.deltaTime);
+	}
+
+	void bombTriggerDelayTimer_onFinish() {
+		print("BOOM");
+	}
+}
---
+++ Assets/Scripts/Util/TimerManager.cs
@@ -0,0 +1,151 @@
+﻿/*
+Written by Geordie Powers
+https://github.com/GeordieP/
+https://gist.github.com/GeordieP/
+MIT License
+*/
+
+/*
+USAGE:
+Create a one-shot timer named "myTimer" with duration 3 seconds, and 
+call a method (return type void) named "myTimer_onFinish" when finished
+
+	Timer myTimer = TimerManager.Instance.CreateTimerOneshot(3.0f);
+	myTimer.onFinish += myTimer_onFinish;
+	myTimer.Start();
+
+To create and start the timer immediately:
+
+	Timer myTimer = TimerManager.Instance.StartTimerOneshot(3.0f);
+	myTimer.onFinish += myTimer_onFinish;
+
+Repeating timers:
+
+	Timer myTimer = TimerManager.Instance.CreateTimerRepeat(3.0f);
+	myTimer.onFinish += myTimer_onFinish;
+	myTimer.Start();
+
+	myTimer.Stop(); // call to stop, and .Start() to start again
+*/
+
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TimerManager : PersistentUnitySingleton<TimerManager> {
+	private List<Timer> timers = new List<Timer>();
+
+	void Update() {
+		if (timers.Count < 1) return;
+
+		for (int i = 0; i < timers.Count; i++) {
+			if (timers[i].running) {
+				timers[i].Update(Time.deltaTime);
+				if (timers[i].finished && timers[i].DeleteOnFinish) {
+					timers.RemoveAt(i);
+					i--;
+				}
+			}
+		}
+	}
+
+	// One shot timers will run until they fire, then stop executing and wait until they're started again
+	// Create a timer and return it
+	public Timer CreateTimerOneshot(float duration) {
+		Timer temp = new Timer(duration);
+		timers.Add(temp);
+		return temp;
+	}
+
+	// Create a timer, start it, and return it
+	public Timer StartTimerOneshot(float duration) {
+		Timer temp = new Timer(duration);
+		timers.Add(temp);
+		temp.Start();
+		return temp;
+	}
+
+	// Temporary one shot timers are the same as one shot timers, but they'll get deleted after they fire once
+	// Create a timer and return it
+	public Timer CreateTimerTemporaryOneshot(float duration) {
+		Timer temp = new Timer(duration);
+		temp.DeleteOnFinish = true;
+		timers.Add(temp);
+		return temp;
+	}
+
+	// Create a timer, start it, and return it
+	public Timer StartTimerTemporaryOneshot(float duration) {
+		Timer temp = new Timer(duration);
+		temp.DeleteOnFinish = true;
+		timers.Add(temp);
+		temp.Start();
+		return temp;
+	}
+
+	// Repeating timers work the exact same way except they don't set the finished and running
+	// flags once they complete, they'll just invoke their callback and run again
+	// Repeating timers won't stop until .Stop() is called
+	public Timer CreateTimerRepeat(float duration) {
+		Timer temp = new Timer(duration);
+		temp.Repeat = true;
+		timers.Add(temp);
+		return temp;
+	}
+
+	// Create a timer, start it, and return it
+	public Timer StartTimerRepeat(float duration) {
+		Timer temp = new Timer(duration);
+		temp.Repeat = true;
+		timers.Add(temp);
+		temp.Start();
+		return temp;
+	}
+}
+
+public class Timer {
+	public float elapsed, duration;
+	public bool running, finished;
+	public delegate void FinishAction();
+	public event FinishAction onFinish;
+
+	private bool deleteOnFinish;
+	public bool DeleteOnFinish {
+		get { return deleteOnFinish; }
+		set { deleteOnFinish = true; }
+	}
+
+	private bool repeat;
+	public bool Repeat {
+		get { return repeat; }
+		set { repeat = true; }
+	}
+
+	public Timer(float duration) {
+		elapsed = 0f;
+		running = false;
+		finished = false;
+		this.duration = duration;
+	}
+
+	public void Start() {
+		running = true;
+	}
+
+	public void Stop() {
+		running = false;
+		elapsed = 0f;
+	}
+
+	public void Update(float deltaTime) {
+		elapsed += deltaTime;
+		if (elapsed >= duration) Finish();
+	}
+
+	public void Finish() {
+		onFinish();
+		elapsed = 0f;
+		if (repeat) return;
+		finished = true;
+		running = false;
+	}
+}
