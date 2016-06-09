9b8ba0621e41be7e149403cc740d709c1cdf08f8
GeordieP <geordie.powers@gmail.com>
2016-06-08 21:26:44 -0400

    Explosion particles now rotate. Removed one not really used Spawn method

--- Assets/Scripts/Gameplay/ExplosionFragment.cs
+++ Assets/Scripts/Gameplay/ExplosionFragment.cs
@@ -7,23 +7,18 @@ public class ExplosionFragment : PooledEntity {
     private float explosionStartTime, timeDiff;
     private const float velocityLimitY = -8f;     // max velocity of particles moving downwards
     private static Vector2 acceleration = new Vector2(0.97f, -0.09f);
+    private float rotationStep;
 
 	private void Start () {
         GetComponent<SpriteRenderer>().sprite = GameManager.Instance.ExplosionFragmentSprites[Random.Range(0, GameManager.Instance.ExplosionFragmentSprites.Length)];
 	}
-
-    public void Spawn(Vector3 origin, float explosionStartTime) {
-        this.explosionStartTime = explosionStartTime;
-        transform.position = origin;
-        MOVE_SPEED = Random.Range(3f, 15f);
-        velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(0.5f, 1.5f), 0f);
-    }
 
     public void Spawn(Vector3 origin, Vector3 directionInfluence, float explosionStartTime) {
         this.explosionStartTime = explosionStartTime;
         transform.position = origin;
         MOVE_SPEED = Random.Range(3f, 15f);
         velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(0.4f, 1.2f), 0f) + directionInfluence;
+        rotationStep = Random.Range(-8f, 8f);
     }
 
     public override void Despawn() {
@@ -35,7 +30,8 @@ public class ExplosionFragment : PooledEntity {
         velocity.x *= acceleration.x;
         if (velocity.y > velocityLimitY) velocity.y += acceleration.y * timeDiff;
 
-        transform.Translate(velocity * MOVE_SPEED * Time.deltaTime);
+        transform.Rotate(new Vector3(0f, 0f, 1f), rotationStep);
+        transform.Translate(velocity * MOVE_SPEED * Time.deltaTime, Space.World);
 
         if (transform.position.y > Balance.ScreenBounds.top || transform.position.y < Balance.ScreenBounds.bottom || transform.position.x < Balance.ScreenBounds.left || transform.position.x > Balance.ScreenBounds.right) {
             GameManager.Instance.ExplosionFragmentReturnToPool(gameObject);
